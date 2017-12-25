5e6522279e3a49ee099ab6110a0c39cfca8f2f09
sadrbox <sadrbox@mail.ru>
2017-12-25 18:21:39 +0000

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <p>Configuration...</p>
             </div>
         </div>
     </body>
